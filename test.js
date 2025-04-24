e3454f9c7bd30c001433dce8aa4b232c06badb18
xeureka <teklemariameureka@gmail.com>
2025-04-24 20:01:26 +0000

    adding test.js on the .gitignore file

---
+++ test.js
@@ -0,0 +1,18 @@
+
+// First thing we do tomorrow is wake up take shower
+
+// then we will gonna understand the working of the lib-way up to second nature
+
+// then we will gonna try to implement in custum way and re write the lib way
+
+// finish the olla UI thing by utilizaing AI
+
+// undertand the code working of olla-backend
+
+// and we will start to learn and understand well the Auth thing
+
+// and we have to make sure we do our best for know
+
+// and you know we will gonna start studying for the final exam starting from next monday 
+
+// lets start to work on (30 hours for the next 3 days)
