0d815f4f4e5d41ab6dc43273a4ab929eb0151dbb
xeureka <teklemariameureka@gmail.com>
2025-04-24 19:34:57 +0000

    library method works

---
+++ routes/urlRoutes.js
@@ -0,0 +1,46 @@
+
+const express = require('express')
+const router = express.Router()
+const Urls = require('../models/url')
+const shortid = require('shortid')
+
+
+router.post('/short', async (req,res) =>{
+
+    const {longUrl} = req.body;
+    const shortCode = shortid.generate()
+
+    try {
+
+        let url = new Urls({
+            longUrl,
+            shortCode
+        })
+
+        await url.save()
+
+        res.json({shortUrl: `http://localhost:3000/${shortCode}`})
+        
+    } catch (error) {
+        res.status(500).json('Server error')
+    }
+})
+
+router.get('/:shortCode', async (req,res) =>{
+
+    try {
+
+        const url = await Urls.findOne({shortCode: req.params.shortCode})
+
+        if (url){
+            return res.redirect(url.longUrl)
+        } else{
+            return res.status(404).json('No URL found !!')
+        }
+        
+    } catch (error) {
+        res.status(500).json('Server error')
+    }
+})
+
+module.exports = router;
