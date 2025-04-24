cb8b36508e91001b3591440e740796ceda90b043
xeureka <teklemariameureka@gmail.com>
2025-04-24 19:43:20 +0000

    some code cleanups

--- index.js
+++ index.js
@@ -2,20 +2,14 @@
 const express = require('express')
 const connectDB = require('./models/connection')
 const app = express()
-const Shorturl = require('./models/shortUrl')
+const cors = require('cors')
+const urlRoutes = require('./routes/urlRoutes')
 
-// app.use(express.urlencoded({ extended: false}))
+app.use(cors())
+app.use(express.json())
 
+app.use('/',urlRoutes)
 
-app.post('/shortUrls',async (req,res) =>{
-
-    const full = "https://www.youtube.com/watch?v=UiGbCPRCz9o&t=2046s&ab_channel=GugutPodcast"
-
-    const shorrty = await Shorturl.find()
-    res.send(shorrty)
-    res.send(full)
-
-})
 
 
 connectDB()
